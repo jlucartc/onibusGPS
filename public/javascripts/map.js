f933d688dbc627b392936cb2b1dc660a91c6db9a
jlucartc <jlucartc@gmail.com>
2019-11-25 16:54:43 -0300

    enviando arquivos

---
+++ public/javascripts/map.js
@@ -0,0 +1,21 @@
+var layer = new L.StamenTileLayer("toner");
+var map = new L.Map("mapid", {
+    center: new L.LatLng(-3.7172200, -38.5430600),
+    zoom: 12
+});
+map.addLayer(layer);
+
+var marker = L.marker([-3.717, -38.543]).addTo(map);
+marker.bindPopup("<b>Hello world!</b><br>I am a popup.");
+
+function getDataFromTTN(){
+
+	//
+
+}
+
+function saveToMongo(){
+
+	//
+
+}
