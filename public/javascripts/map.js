d847d33dc93d92dd621dfc7a0c463be004c4b0b9
jlucartc <jlucartc@gmail.com>
2019-11-27 22:57:53 -0300

    Finalizando consumo de dados da TTN via Swagger

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -1,19 +1,3 @@
-var xhttp = new XMLHttpRequest(); 
-
-
-xhttp.onreadystatechange = function() {
-  if (this.readyState == 4 && this.status == 200) {
-    console.log(this.responseText);
-  }
-};
-xhttp.open("GET","https://bus_gps_data.data.thethingsnetwork.org/api/v2/devices", true);
-
-//xhttp.setRequestHeader('Accept','application/json');
-xhttp.setRequestHeader('Content-Type', 'application/json');
-xhttp.setRequestHeader('Authorization','Basic key ttn-account-v2.IFJLbrAmGz8RoPRF-_DF0MRiIp-hmtB2it2HRdn9_yA');
-
-xhttp.send(); 
-
 var layer = new L.StamenTileLayer("toner");
 var map = new L.Map("mapid", {
     center: new L.LatLng(-3.7172200, -38.5430600),
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,41 @@
 var express = require('express');
 const http = require('http');
+const cp = require("child_process");
 
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-  res.render('index', { title: 'Express' });
+  var data = '123';
+
+
+  var command = "curl -X GET --header 'Accept: application/json' --header 'Authorization: key ttn-account-v2.AXVZUWtEus1MMpVF8qGf8a7jQEbkU4sUA9sM3WsGkDI' 'https://bus_gps_data.data.thethingsnetwork.org/api/v2/query?last=7d'";
+
+  cp.exec(command,function(err,stdout,stderr){
+  	
+  	if(err){ 
+
+  		console.log('error');
+
+  	}else{
+
+  		console.log("Tipo: "+typeof(stdout));
+
+  		var data = JSON.parse(stdout);
+
+  		var raw = data[data.length-1].raw.split("/");
+
+  		console.log("Dado: "+raw);
+
+  		res.render('index', { title: 'Express' , data: raw });
+
+  	} 
+  
+  });
+
+  
+
 });
 
 
